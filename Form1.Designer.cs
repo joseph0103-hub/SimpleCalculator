dded879933d371a3160a67f1e1ef580cbd7554eb
user <user@DESKTOP-TJJTNJJ>
2026-03-26 17:16:07 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace SimpleCalculator
+{
+    partial class frmCalculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblSimpleCalculator = new Label();
+            txtInputWindow = new TextBox();
+            txtOutputWindow = new TextBox();
+            btnEditCE = new Button();
+            btnNum0 = new Button();
+            btnNum2 = new Button();
+            btnFuncPlus = new Button();
+            btnNum1 = new Button();
+            btnNum5 = new Button();
+            btnNum4 = new Button();
+            btnNum8 = new Button();
+            btnNum7 = new Button();
+            btnEditC = new Button();
+            btnOpEql = new Button();
+            btnNum9 = new Button();
+            btnNum6 = new Button();
+            btnNum3 = new Button();
+            btnFuncDot = new Button();
+            btnEditDel = new Button();
+            btnOpDiv = new Button();
+            btnOpMul = new Button();
+            btnOpSub = new Button();
+            btnOpAdd = new Button();
+            SuspendLayout();
+            // 
+            // lblSimpleCalculator
+            // 
+            lblSimpleCalculator.AutoSize = true;
+            lblSimpleCalculator.Font = new Font("맑은 고딕", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            lblSimpleCalculator.ForeColor = Color.Red;
+            lblSimpleCalculator.Location = new Point(36, 19);
+            lblSimpleCalculator.Name = "lblSimpleCalculator";
+            lblSimpleCalculator.Size = new Size(350, 50);
+            lblSimpleCalculator.TabIndex = 0;
+            lblSimpleCalculator.Text = "　Simple Calculator";
+            // 
+            // txtInputWindow
+            // 
+            txtInputWindow.Location = new Point(36, 81);
+            txtInputWindow.Name = "txtInputWindow";
+            txtInputWindow.Size = new Size(437, 23);
+            txtInputWindow.TabIndex = 1;
+            // 
+            // txtOutputWindow
+            // 
+            txtOutputWindow.BackColor = Color.Lime;
+            txtOutputWindow.Location = new Point(36, 129);
+            txtOutputWindow.Name = "txtOutputWindow";
+            txtOutputWindow.Size = new Size(437, 23);
+            txtOutputWindow.TabIndex = 2;
+            // 
+            // btnEditCE
+            // 
+            btnEditCE.BackColor = Color.FromArgb(255, 255, 128);
+            btnEditCE.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnEditCE.Location = new Point(36, 178);
+            btnEditCE.Name = "btnEditCE";
+            btnEditCE.Size = new Size(96, 48);
+            btnEditCE.TabIndex = 3;
+            btnEditCE.Text = "CE";
+            btnEditCE.UseVisualStyleBackColor = false;
+            // 
+            // btnNum0
+            // 
+            btnNum0.BackColor = Color.FromArgb(128, 255, 128);
+            btnNum0.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnNum0.Location = new Point(153, 395);
+            btnNum0.Name = "btnNum0";
+            btnNum0.Size = new Size(96, 48);
+            btnNum0.TabIndex = 4;
+            btnNum0.Text = "0";
+            btnNum0.UseVisualStyleBackColor = false;
+            // 
+            // btnNum2
+            // 
+            btnNum2.BackColor = Color.FromArgb(128, 255, 128);
+            btnNum2.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnNum2.Location = new Point(153, 341);
+            btnNum2.Name = "btnNum2";
+            btnNum2.Size = new Size(96, 48);
+            btnNum2.TabIndex = 5;
+            btnNum2.Text = "2";
+            btnNum2.UseVisualStyleBackColor = false;
+            // 
+            // btnFuncPlus
+            // 
+            btnFuncPlus.BackColor = Color.FromArgb(128, 128, 255);
+            btnFuncPlus.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnFuncPlus.Location = new Point(36, 395);
+            btnFuncPlus.Name = "btnFuncPlus";
+            btnFuncPlus.Size = new Size(96, 48);
+            btnFuncPlus.TabIndex = 6;
+            btnFuncPlus.Text = "+/-";
+            btnFuncPlus.UseVisualStyleBackColor = false;
+            // 
+            // btnNum1
+            // 
+            btnNum1.BackColor = Color.FromArgb(128, 255, 128);
+            btnNum1.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnNum1.Location = new Point(36, 341);
+            btnNum1.Name = "btnNum1";
+            btnNum1.Size = new Size(96, 48);
+            btnNum1.TabIndex = 7;
+            btnNum1.Text = "1";
+            btnNum1.UseVisualStyleBackColor = false;
+            // 
+            // btnNum5
+            // 
+            btnNum5.BackColor = Color.FromArgb(128, 255, 128);
+            btnNum5.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnNum5.Location = new Point(153, 287);
+            btnNum5.Name = "btnNum5";
+            btnNum5.Size = new Size(96, 48);
+            btnNum5.TabIndex = 8;
+            btnNum5.Text = "５";
+            btnNum5.UseVisualStyleBackColor = false;
+            // 
+            // btnNum4
+            // 
+            btnNum4.BackColor = Color.FromArgb(128, 255, 128);
+            btnNum4.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnNum4.Location = new Point(36, 287);
+            btnNum4.Name = "btnNum4";
+            btnNum4.Size = new Size(96, 48);
+            btnNum4.TabIndex = 9;
+            btnNum4.Text = "４";
+            btnNum4.UseVisualStyleBackColor = false;
+            // 
+            // btnNum8
+            // 
+            btnNum8.BackColor = Color.FromArgb(128, 255, 128);
+            btnNum8.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnNum8.Location = new Point(153, 233);
+            btnNum8.Name = "btnNum8";
+            btnNum8.Size = new Size(96, 48);
+            btnNum8.TabIndex = 10;
+            btnNum8.Text = "８";
+            btnNum8.UseVisualStyleBackColor = false;
+            // 
+            // btnNum7
+            // 
+            btnNum7.BackColor = Color.FromArgb(128, 255, 128);
+            btnNum7.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnNum7.Location = new Point(36, 233);
+            btnNum7.Name = "btnNum7";
+            btnNum7.Size = new Size(96, 48);
+            btnNum7.TabIndex = 11;
+            btnNum7.Text = "７";
+            btnNum7.UseVisualStyleBackColor = false;
+            // 
+            // btnEditC
+            // 
+            btnEditC.BackColor = Color.FromArgb(255, 255, 128);
+            btnEditC.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnEditC.Location = new Point(153, 178);
+            btnEditC.Name = "btnEditC";
+            btnEditC.Size = new Size(96, 48);
+            btnEditC.TabIndex = 12;
+            btnEditC.Text = "Ｃ";
+            btnEditC.UseVisualStyleBackColor = false;
+            // 
+            // btnOpEql
+            // 
+            btnOpEql.BackColor = Color.FromArgb(255, 128, 255);
+            btnOpEql.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnOpEql.Location = new Point(377, 395);
+            btnOpEql.Name = "btnOpEql";
+            btnOpEql.Size = new Size(96, 48);
+            btnOpEql.TabIndex = 13;
+            btnOpEql.Text = "=";
+            btnOpEql.UseVisualStyleBackColor = false;
+            btnOpEql.Click += button11_Click;
+            // 
+            // btnNum9
+            // 
+            btnNum9.BackColor = Color.FromArgb(128, 255, 128);
+            btnNum9.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnNum9.Location = new Point(266, 232);
+            btnNum9.Name = "btnNum9";
+            btnNum9.Size = new Size(96, 48);
+            btnNum9.TabIndex = 14;
+            btnNum9.Text = "９";
+            btnNum9.UseVisualStyleBackColor = false;
+            // 
+            // btnNum6
+            // 
+            btnNum6.BackColor = Color.FromArgb(128, 255, 128);
+            btnNum6.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnNum6.Location = new Point(266, 287);
+            btnNum6.Name = "btnNum6";
+            btnNum6.Size = new Size(96, 48);
+            btnNum6.TabIndex = 15;
+            btnNum6.Text = "６";
+            btnNum6.UseVisualStyleBackColor = false;
+            // 
+            // btnNum3
+            // 
+            btnNum3.BackColor = Color.FromArgb(128, 255, 128);
+            btnNum3.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnNum3.Location = new Point(266, 341);
+            btnNum3.Name = "btnNum3";
+            btnNum3.Size = new Size(96, 48);
+            btnNum3.TabIndex = 16;
+            btnNum3.Text = "3";
+            btnNum3.UseVisualStyleBackColor = false;
+            // 
+            // btnFuncDot
+            // 
+            btnFuncDot.BackColor = Color.FromArgb(128, 128, 255);
+            btnFuncDot.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnFuncDot.Location = new Point(266, 395);
+            btnFuncDot.Name = "btnFuncDot";
+            btnFuncDot.Size = new Size(96, 48);
+            btnFuncDot.TabIndex = 17;
+            btnFuncDot.Text = ".";
+            btnFuncDot.UseVisualStyleBackColor = false;
+            // 
+            // btnEditDel
+            // 
+            btnEditDel.BackColor = Color.FromArgb(255, 255, 128);
+            btnEditDel.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnEditDel.Location = new Point(266, 178);
+            btnEditDel.Name = "btnEditDel";
+            btnEditDel.Size = new Size(96, 48);
+            btnEditDel.TabIndex = 18;
+            btnEditDel.Text = "Del";
+            btnEditDel.UseVisualStyleBackColor = false;
+            // 
+            // btnOpDiv
+            // 
+            btnOpDiv.BackColor = Color.FromArgb(255, 128, 255);
+            btnOpDiv.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnOpDiv.Location = new Point(377, 178);
+            btnOpDiv.Name = "btnOpDiv";
+            btnOpDiv.Size = new Size(96, 48);
+            btnOpDiv.TabIndex = 19;
+            btnOpDiv.Text = "÷";
+            btnOpDiv.UseVisualStyleBackColor = false;
+            // 
+            // btnOpMul
+            // 
+            btnOpMul.BackColor = Color.FromArgb(255, 128, 255);
+            btnOpMul.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnOpMul.Location = new Point(377, 233);
+            btnOpMul.Name = "btnOpMul";
+            btnOpMul.Size = new Size(96, 48);
+            btnOpMul.TabIndex = 20;
+            btnOpMul.Text = "×";
+            btnOpMul.UseVisualStyleBackColor = false;
+            // 
+            // btnOpSub
+            // 
+            btnOpSub.BackColor = Color.FromArgb(255, 128, 255);
+            btnOpSub.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnOpSub.Location = new Point(377, 287);
+            btnOpSub.Name = "btnOpSub";
+            btnOpSub.Size = new Size(96, 48);
+            btnOpSub.TabIndex = 21;
+            btnOpSub.Text = "－";
+            btnOpSub.UseVisualStyleBackColor = false;
+            // 
+            // btnOpAdd
+            // 
+            btnOpAdd.BackColor = Color.FromArgb(255, 128, 255);
+            btnOpAdd.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnOpAdd.Location = new Point(377, 341);
+            btnOpAdd.Name = "btnOpAdd";
+            btnOpAdd.Size = new Size(96, 48);
+            btnOpAdd.TabIndex = 22;
+            btnOpAdd.Text = "＋";
+            btnOpAdd.UseVisualStyleBackColor = false;
+            // 
+            // frmCalculator
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(531, 468);
+            Controls.Add(btnOpAdd);
+            Controls.Add(btnOpSub);
+            Controls.Add(btnOpMul);
+            Controls.Add(btnOpDiv);
+            Controls.Add(btnEditDel);
+            Controls.Add(btnFuncDot);
+            Controls.Add(btnNum3);
+            Controls.Add(btnNum6);
+            Controls.Add(btnNum9);
+            Controls.Add(btnOpEql);
+            Controls.Add(btnEditC);
+            Controls.Add(btnNum7);
+            Controls.Add(btnNum8);
+            Controls.Add(btnNum4);
+            Controls.Add(btnNum5);
+            Controls.Add(btnNum1);
+            Controls.Add(btnFuncPlus);
+            Controls.Add(btnNum2);
+            Controls.Add(btnNum0);
+            Controls.Add(btnEditCE);
+            Controls.Add(txtOutputWindow);
+            Controls.Add(txtInputWindow);
+            Controls.Add(lblSimpleCalculator);
+            Name = "frmCalculator";
+            Text = "Calculator v1.0";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblSimpleCalculator;
+        private TextBox txtInputWindow;
+        private TextBox txtOutputWindow;
+        private Button btnEditCE;
+        private Button btnNum0;
+        private Button btnNum2;
+        private Button btnFuncPlus;
+        private Button btnNum1;
+        private Button btnNum5;
+        private Button btnNum4;
+        private Button btnNum8;
+        private Button btnNum7;
+        private Button btnEditC;
+        private Button btnOpEql;
+        private Button btnNum9;
+        private Button btnNum6;
+        private Button btnNum3;
+        private Button btnFuncDot;
+        private Button btnEditDel;
+        private Button btnOpDiv;
+        private Button btnOpMul;
+        private Button btnOpSub;
+        private Button btnOpAdd;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace SimpleCalculator
+{
+    public partial class frmCalculator : Form
+    {
+        public frmCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
